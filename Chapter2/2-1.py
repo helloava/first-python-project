e4c69e52dd7381c7a487bb162661f68c8fb569c8
ahyoung <helloavakoo@gmail.com>
2026-01-01 22:10:11 +0900

    second commit

--- Chapter2/2-1.py
+++ Chapter2/2-1.py
@@ -1,5 +1,44 @@
 a='I\'m hungry'
 print(a)
+
 head="python"
 tail=" is fun!"
 print(head + tail)
+print(head)
+
+print("="*50)
+len(head)
+a="Life is too short, You need Python"
+print(a[0])
+print(a[12])
+print(a[1])
+print(a[-3])
+b=a[0:4]
+print(b)
+print(a[1:4])
+print(a[0:4])
+print(a[::-1])
+a="I eat %d apples" %10
+print(a)
+
+number="three"
+a="I eat %s apples" %number
+print(a)
+number=3
+day="three"
+a="I ate %d%% apples, I sick for %s days" %(number, day)
+print(a)
+
+a="농도는 %10.1f 임" %0.4
+print(a)
+
+number=100
+day="hundred"
+a = f"I eat {day} apples. I was sick for {day} days"
+
+print(a)
+a="{:+^9}".format("hi")
+print(a)
+a=f'{"hi":>40}'
+print(a)
+
